41c7839c1a09fe5576822fc013a570926dd7e754
Gobinath <slgobinath@gmail.com>
2017-10-19 10:34:09 -0400

    Add more commands

--- auto_configure_manjaro.sh
+++ auto_configure_manjaro.sh
@@ -5,3 +5,8 @@ yaourt -S tilix --noconfirm
 
 echo "Install Visual Studio Code"
 yaourt -S visual-studio-code --noconfirm
+
+echo "Install Python modules"
+sudo apt install python3-pip python3-dev python-virtualenv python3-setuptools
+
+sudo pip3 install pylint
--- auto_configure_mint.sh
+++ auto_configure_mint.sh
@@ -6,6 +6,9 @@ sudo add-apt-repository ppa:webupd8team/terminix -y
 echo "Update the repositories"
 sudo apt update
 
+echo "Install Snap"
+sudo apt install snapd
+
 echo "Install Git"
 sudo apt install git
 
@@ -17,15 +20,30 @@ sudo apt install tilix
 echo "Install Vim editor"
 sudo apt install vim
 
+echo "Install tree command"
+sudo apt install tree
+
 echo "Install Visual Studio Code"
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-sudo apt-get update
-sudo apt-get install code
+sudo apt update
+sudo apt install code
+
+echo "Install Sublime Text"
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt update
+sudo apt-get install sublime-text
 
 echo "Install Python modules"
-sudo apt install python3-pip python3-dev python-virtualenv
+sudo apt install python3-pip python3-dev python-virtualenv python3-setuptools
+
+sudo pip3 install pylint
+
+echo "Install magicwormhole"
+sudo snap install wormhole
 
 echo "Install themes"
 # Install capitaine-cursors
@@ -43,3 +61,39 @@ mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vi
 cd ~/.vim/bundle
 git clone https://github.com/tpope/vim-sensible.git
 git clone https://github.com/ervandew/supertab.git
+
+echo "Install synapse"
+sudo apt install synapse
+
+echo "Install SMPlayer"
+sudo add-apt-repository ppa:rvm/smplayer 
+sudo apt update 
+sudo apt install smplayer smplayer-themes smplayer-skins
+
+echo "Install VLC"
+sudo apt install vlc browser-plugin-vl
+
+# Install build tools
+echo "Install cmake"
+sudo apt install cmake
+
+# Install download manager
+echo "Install aria2"
+sudo apt install aria2
+
+echo "Install uGet Download Manager"
+sudo add-apt-repository ppa:plushuang-tw/uget-stable
+sudo apt update
+sudo apt install uget
+
+echo "Install uGet Chrome Wrapper"
+sudo add-apt-repository ppa:slgobinath/uget-chrome-wrapper
+sudo apt update
+sudo apt install uget-chrome-wrapper
+
+echo "Install shutter"
+sudo add-apt-repository ppa:shutter/ppa
+sudo apt update && sudo apt install shutter
+# Missing dependency in Linux Mint
+# @See https://itsfoss.com/shutter-edit-button-disabled/
+sudo apt install libgoo-canvas-perl
